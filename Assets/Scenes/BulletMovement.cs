4d5507571e7fcd8eccbabc9ba71a81cdc26787ad
Bruno Rico <54558283+AinzOolGown@users.noreply.github.com>
2019-10-06 10:07:47 -0400

    Scripts that will determine bullet waves
    Bullet Movement - applied to the actual bullet prefab/object that will be spawned, apply variables before hand
    
    Bullet Wave - determines what will be spawned when, where, and in what direction. Can currently do one bullet at a time.

---
+++ Assets/Scenes/BulletMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+
+    public Transform bullet;
+    public float Yvelocity; 
+    public float Xvelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement on the y axis relative to the rotation of the object
+        transform.position += transform.up * Time.deltaTime * Yvelocity;
+
+        //movement on the x axis relative to the rotation of the object
+        transform.position += transform.right * Time.deltaTime * Xvelocity;
+    
+    }
+}
---
+++ Assets/Scenes/BulletWave.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletWave : MonoBehaviour
+{
+    public float timeDelay = 0.0f;
+    public GameObject box;  //object you want spawned goes in here
+    public float xAxis = 0.0f;  // Determines where it will spawn in the x axis
+    public float yAxis = 0.0f;  // Determines where it will spawn in the y axis
+    public float zRotation = 0.0f;  // Rotation counter clockwise 
+    
+    private float xRotation = 0.0f;  
+    private float yRotation = 0.0f;
+    private float amTime = 0.0f; 
+    
+
+
+    void Start()
+    {
+        StartCoroutine("Timer");
+    }
+
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        Quaternion angle = Quaternion.Euler(xRotation,yRotation,zRotation); // creating angle variable for object
+        Vector3 postition = new Vector3(xAxis,yAxis,0.0f);  // creating position variable for object
+
+        Debug.Log("Its here"); //check to see if it works
+
+        Instantiate(box, postition, angle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amTime = amTime + Time.deltaTime;       
+        
+    }
+}
